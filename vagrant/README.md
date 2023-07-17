0a0258f79f99a4a619c287a314c343862355bcd0
Le Long <longromsay@gmail.com>
2023-07-17 11:07:31 +0700

    [VAGRANT] Add documentation for create virual box with vagrant
    - Add installation file for install Docker, Kubernetes, etc in virtual machine
    - Add Vagrantfile for master node
    - Add README.md file for vagrant folder
    - Add image resources for vagrant documentations

---
+++ vagrant/README.md
@@ -0,0 +1,48 @@
+# Documentation for creating a virtual machine with ubuntu/jammy64 based image by vagrant
+
+## Quick start ⚡
+- [Prerequisites](#prerequisites)
+- [Create virtual machine](#virtual-machine)
+- [Explanation](#explanation)
+
+## PREREQUISITES
+[Download and install virtualbox](https://www.virtualbox.org/wiki/Downloads)
+
+## VIRTUAL-MACHINE
+- Open the terminal window.
+- Type commands below into the terminal window to create a virtual machine.
+```bash
+cd vagrant/master
+chmod +x ../install-docker-kube.sh
+vagrant up
+```
+
+## EXPLANATION
+- The `install-docker-kube.sh` file contains all necessarily libraries like Docker, Kubernetes, etc.
+- Basic settings for virtual machine
+```bash
+Vagrant.configure("2") do |config|
+  config.vm.box = "ubuntu/jammy64"
+  config.vm.network "public_network", bridge: "enp5s0", ip: "192.168.1.200"
+  config.vm.hostname = "master.rainscales"
+  config.ssh.insert_key = false
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.name = "master.rainscales"
+    vb.cpus = 4
+    vb.memory = "5120"
+  end
+```
+We're using static IP (192.168.1.200) for virtual machine. The setting can be `config.vm.network "private_network", ip: "192.168.1.200` in the case you are testing with your laptop in home network. <br>
+If it's not work after ssh to the virtual machine. Using `ifconfig` (ubuntu), find your network interface (`enp5s0` in this case) and using the above setting with `public_network`.
+![Network interface](../resources/images/vagrant/network_interface.png) <br><br>
+The master node requires a minimum of **2 cores** and **2 GB** of memory. <br><br>
+Using `config.vm.provision` to install or modify some other settings (example: set password for root account, etc). <br><br>
+We need to modify the /etc/hosts file and add an additional line as shown below
+```bash
+# Add entries to /etc/hosts file for accessing machines via hostname
+cat >> /etc/hosts << EOF
+192.168.1.200 master.rainscales
+EOF
+```
+If your cluster has N nodes, we need to modify the /etc/hosts file on each node and add the hostname for all nodes. This allows each node to identify the hostnames of the other nodes.
---
+++ vagrant/install-docker-kube.sh
@@ -0,0 +1,82 @@
+export DEBIAN_FRONTEND=noninteractive
+# Tat swap
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+swapoff -a
+
+# Install docker
+apt-get update
+apt-get install ca-certificates curl gnupg -y
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+
+
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+systemctl enable docker.service
+systemctl daemon-reload
+systemctl restart docker
+
+
+
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboot
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sysctl --system >/dev/null 2>&1
+
+
+
+
+mkdir -p /etc/apt/keyrings
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+
+apt-get update
+
+apt-get install -y  kubelet=1.26.2-00 kubeadm=1.26.2-00 kubectl=1.26.2-00
+apt-mark hold kubelet kubeadm kubectl
+
+
+containerd config default > /etc/containerd/config.toml
+sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
+systemctl restart containerd
+systemctl restart kubelet
